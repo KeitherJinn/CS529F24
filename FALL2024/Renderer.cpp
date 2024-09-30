fe2efa2dd5e6afc1cbde7bf45665d9fec4055b57
KeitherJinn <80877899+KeitherJinn@users.noreply.github.com>
2024-09-30 14:16:32 -0700

    Assignment3 Part2

--- FALL2024/Renderer.cpp
+++ FALL2024/Renderer.cpp
@@ -42,6 +42,86 @@ void Renderer::frameBufferSizeCallback(GLFWwindow* w, int l, int h) {
 	glViewport(0, 0, l, h);
 }
 
-Triangle Renderer::drawTriangle(Vector3 v1, Vector3 v2, Vector3 v3) {
-	return Triangle(v1, v2, v3);
+unsigned int Renderer::drawTriangle(Vector3 v1, Vector3 v2, Vector3 v3) {
+	float vertices[] = {
+	v1.x, v1.y, v1.z,
+	v2.x, v2.y, v2.z,
+	v3.x, v3.y, v3.z,
+	};
+	unsigned int vertexShader = initializeVertexShader();
+	unsigned int fragmentShader = initializeFragmentShader();
+	unsigned int shaderProgram = buildShaderProgram(vertexShader, fragmentShader);
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	unsigned int VBO, VAO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	return shaderProgram;
 }
+
+unsigned int Renderer::initializeVertexShader() {
+	const char* vertexShaderSource = "#version 330 core\n"
+		"layout (location = 0) in vec3 aPos;\n"
+		"void main()\n"
+		"{\n"
+		"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+		"}\0";
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	int  success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+	return vertexShader;
+}
+
+unsigned int Renderer::initializeFragmentShader() {
+	const char* fragmentShaderSource = "#version 330 core\n"
+		"out vec4 FragColor;\n"
+		"void main()\n"
+		"{\n"
+		"    FragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);\n"
+		"}\0";
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	int success;
+	char infoLog[512];
+	glCompileShader(fragmentShader);
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+	return fragmentShader;
+}
+
+unsigned int Renderer::buildShaderProgram(unsigned int vertexShader, unsigned int fragmentShader) {
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	int success;
+	char infoLog[512];
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+	return shaderProgram;
+}
+
--- FALL2024/Renderer.h
+++ FALL2024/Renderer.h
@@ -2,17 +2,20 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include "GameWindow.h"
-#include "Triangle.h"
+#include "Vector3.h"
 
 class Renderer {
 public:
 	Renderer(GameWindow& Window);
 	void clear(float r, float g, float b, float a);
 	void swapBuffers();
-	Triangle drawTriangle(Vector3 v1, Vector3 v2, Vector3 v3);
+	unsigned int drawTriangle(Vector3 v1, Vector3 v2, Vector3 v3);
 private:
 	void initialize();
 	void setupCallbacks();
 	void frameBufferSizeCallback(GLFWwindow* w, int l, int h);
+	unsigned int initializeVertexShader();
+	unsigned int initializeFragmentShader();
+	unsigned int buildShaderProgram(unsigned int vertexShader, unsigned int fragmentShader);
 	GameWindow& window;
 };
